8d01d8937d0182636a12f669e5c9dc03834e12ef
swati <you@example.com>
2023-06-22 10:44:06 +0530

    java basic

---
+++ _0Java_Basics.java
@@ -0,0 +1,13 @@
+package com.java.PW;
+
+public class _0Java_Basics {
+
+	public static void main(String[] args) {
+		
+		int a= 5;
+		int b= a++ + ++a + ++a + a++ + --a + a--;
+		System.out.println(a);
+		System.out.println(b);
+		
+	}
+}
